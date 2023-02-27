ee56963085a5cfe318d587f67f2f8fd70e3a385d
evertondrums.2@gmail.com <evertondrums.2@gmail.com>
2023-02-27 13:32:52 -0300

    DOM-01

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+(function(){
+//  let pgr = document.getElementById('p')
+  let pgr = document.querySelector('#p')
+  
+  pgr.innerText = 'este é um teste'
+  
+  let tst = document.getElementById('test')
+ 
+ tst.style.backgroundColor = 'green'
+ 
+})()
